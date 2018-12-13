c6aac7cce65d1358a2bbcfcbe2de6e16a30336c4
Allister Rooke <allisterg@gmail.com>
2018-12-13 12:40:44 -0500

    insertbefore and insertafter

--- DSA-LinkedList-thomas-allister.js
+++ DSA-LinkedList-thomas-allister.js
@@ -19,12 +19,13 @@ class LinkedList {
   insertLast(item) {
     if (this.head === null) {
       this.insertFirst(item);
+    } else {
+      let tempNode = this.head;
+      while (tempNode.next !== null) {
+        tempNode = tempNode.next;
+      }
+      tempNode.next = new _Node(item, null);
     }
-    let tempNode = this.head;
-    while (this.next !== null) {
-      tempNode = tempNode.next;
-    }
-    tempNode.next = new _Node(item, null);
   }
 
   remove(item) {
@@ -37,7 +38,7 @@ class LinkedList {
     }
     let current = this.head;
     let previous = this.head;
-    while (current.value !== item && current !== null) {
+    while (current !== null && current.value !== item) {
       previous = current;
       current = current.next;
     }
@@ -52,7 +53,7 @@ class LinkedList {
       return console.error('No items in list!');
     }
     let current = this.head;
-    while (current.value !== item && current !== null) {
+    while (current !== null && current.value !== key) {
       current = current.next;
     }
     if (current === null) {
@@ -60,4 +61,66 @@ class LinkedList {
     }
     return current;
   }
+
+  insertBefore(item, key) {
+    if (this.head.value === key) {
+      this.head = new _Node(key, this.head.next)
+    } else {
+      let current = this.head
+      let previous = this.head
+      while (current !== null && current.value !== key) {
+        previous = current;
+        current = current.next;
+      }
+      if (current === null) {
+        return console.log('Item not found')
+      }
+      previous.next = new _Node(item, current)
+    }
+  }
+
+  insertAfter(item, key) {
+    let current = this.head;
+    while (current !== null && current.value !== key) {
+      current = current.next;
+    }
+    if (current === null ) {
+      return console.log('Item not found');
+    }
+    current.next = new _Node(item, current.next)
+  }
+
 }
+
+function main() {
+
+  let SLL = new LinkedList();
+  SLL.insertFirst('Apollo');
+  SLL.insertLast('Boomer');
+  SLL.insertLast('Helo');
+  SLL.insertLast('Husker');
+  SLL.insertLast('Starbuck');
+
+  // console.log(JSON.stringify(SLL, null, 2));
+
+  SLL.insertLast('Tauhida');
+
+  // console.log(JSON.stringify(SLL, null, 2));
+
+  SLL.remove('squirrel');
+
+  SLL.insertBefore('Athena', 'Boomer');
+
+  // console.log(JSON.stringify(SLL, null, 2));
+
+  SLL.insertAfter('Hotdog', 'Helo');
+
+  console.log(JSON.stringify(SLL, null, 2));
+}
+
+main();
+
+
+
+
+
